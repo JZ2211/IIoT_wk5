23fc9871c0d11872ed24b4429333b0ab524600ff
JZ2211 <100505718+JZ2211@users.noreply.github.com>
2023-08-08 14:50:16 -0500

    Update broken.py

--- LocalSite/broken.py
+++ LocalSite/broken.py
@@ -1,8 +1,10 @@
-# Check if the requested sensor data log is available.
-# Return True (i.e. broken) if the folder/file does not exist.
-#
-# Modified 7/15/2023
-# by Jin Zhu
+"""
+ Check if the requested sensor data log is available.
+ Return True (i.e. broken) if the folder/file does not exist.
+
+ Modified 7/15/2023
+ by Jin Zhu
+"""
 
 import os
 
