fe9a8a854d148ca073cdeb447f57831b3865111c
JZ2211 <100505718+JZ2211@users.noreply.github.com>
2024-03-04 21:57:40 -0600

    Update broken.py

--- LocalSite/broken.py
+++ LocalSite/broken.py
@@ -2,7 +2,7 @@
  Check if the requested sensor data log is available.
  Return True (i.e. broken) if the folder/file does not exist.
 
- Modified 7/15/2023
+ Revised 3/4/2024
  by Jin Zhu
 """
 
@@ -11,8 +11,10 @@ import os
 def broken(sensor_id):
     return not os.path.isdir(sensor_id)
 
-def brokenDate(sensor_id,date):
-    if os.path.isdir(sensor_id):
+def brokenDate(date, sensor_id =""):
+    if (sensor_id==""):   #if not sensor_id is provided, assume log files are stored directly under homepath
+        return not os.path.isfile("log_"+date+".txt")
+    elif os.path.isdir(sensor_id):  #otherwise check if the sensor_id directory exists
         return not os.path.isfile(sensor_id+"/"+sensor_id+"_log_"+date+".txt")
     else:
         return True
